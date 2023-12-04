227338fb30c8bed65f15b1acea89aceb85341cc6
can bingöl <114669146+canbingol@users.noreply.github.com>
2023-12-04 11:48:17 +0300

    updated
    bağlı listelerde işlemler yapıldı

---
+++ VeriYapilari/VeriYapilari/Liste.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeriYapilari
+{
+    internal class Liste
+    {
+      public   Node head;
+        public void liste()
+        {
+
+            if (head == null)
+            {
+                Console.WriteLine(" Liste Boş");
+            }
+
+        }
+        public void SonaEkle(int data)
+        {
+            Node eleman = new Node(data);
+
+            if (head == null)
+            {
+                head = eleman;
+                Console.WriteLine(" İlk Düğüm Eklendi ");
+            }
+            else
+            {
+                Node temp = head;
+
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = eleman;
+                Console.WriteLine("Sona Düğüm Eklendi ");
+
+            }
+
+
+        }
+        public void BasaEkle(int data)
+        {
+            Node temp = new Node(data);
+            if (head == null)
+            {
+                head = temp;
+                Console.WriteLine("Liste Oluşturuldu, İlk Değer Eklendi");
+            }else
+            {
+                temp.next = head;
+                head = temp;
+                Console.WriteLine(" Başa Ekleme Yapıldı");
+            }
+
+
+        }
+        public void Yazdır()
+        {
+            Node temp = head;
+            if (head == null)
+            {
+                Console.WriteLine(" Liste Boş");
+            }
+            else
+            {
+
+                Console.Write(" Baş --> ");
+                while (temp.next != null)
+                {
+                    Console.Write(temp.data + "-->");
+                    temp = temp.next;
+                }
+                Console.WriteLine(temp.data + " Son");
+            }
+        }
+        
+
+    }
+}
---
+++ VeriYapilari/VeriYapilari/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeriYapilari
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Liste list = new Liste();
+            list.liste();
+            list.SonaEkle(10);
+            list.SonaEkle(5);
+            list.SonaEkle(20);
+            list.SonaEkle(50);
+            list.Yazdır();
+            list.BasaEkle(45);
+            list.Yazdır();
+
+
+            Console.ReadKey();
+        }
+
+
+
+
+
+
+
+    }
+}
